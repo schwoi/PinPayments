8592f542702396c64996d8f3f12f2059fdd3a97e
Brent Howard <brent@mydynamics.io>
2018-04-12 21:49:04 +1000

    Commented out the Mapper Class

--- PinPayments/Mapper.cs
+++ PinPayments/Mapper.cs
@@ -1,32 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
 
 namespace PinPayments
 {
     public static class Mapper<T>
     {
-        public static List<T> MapCollectionFromJson(string json, string token = "data")
-        {
-            var list = new List<T>();
+        //public static List<T> MapCollectionFromJson(string json, string token = "data")
+        //{
+        //    var list = new List<T>();
 
-            var jObject = JObject.Parse(json);
+        //    var jObject = JObject.Parse(json);
 
-            var allTokens = jObject.SelectToken(token);
-            foreach (var tkn in allTokens)
-                list.Add(Mapper<T>.MapFromJson(tkn.ToString()));
+        //    var allTokens = jObject.SelectToken(token);
+        //    foreach (var tkn in allTokens)
+        //        list.Add(Mapper<T>.MapFromJson(tkn.ToString()));
 
-            return list;
-        }
+        //    return list;
+        //}
 
-        public static T MapFromJson(string json, string parentToken = null)
-        {
-            var jsonToParse = string.IsNullOrEmpty(parentToken) ? json : JObject.Parse(json).SelectToken(parentToken).ToString();
+        //public static T MapFromJson(string json, string parentToken = null)
+        //{
+        //    var jsonToParse = string.IsNullOrEmpty(parentToken) ? json : JObject.Parse(json).SelectToken(parentToken).ToString();
 
-            return JsonConvert.DeserializeObject<T>(jsonToParse);
-        }
+        //    //return JsonConvert.DeserializeObject<T>(jsonToParse);
+
+        //    using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(jsonToParse)))
+        //    {
+        //        DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(T));
+        //        object objResponse = jsonSerializer.ReadObject(ms);
+        //        T result = (T)objResponse;
+
+        //        return result;
+        //    }
+        //}
     }
 }
